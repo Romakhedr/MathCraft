fc0ecda8f7da10c02ae68070a47c102836573808
Rehamkhedr <romakhedr99@gmail.com>
2026-08-06 05:01:15 +0300

    Delete src/lib/supabase.js

--- src/lib/supabase.js
+++
@@ -1,7 +0,0 @@
-// src/lib/supabase.js
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://fpfnhgduyzlgmliuvmqu.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'YOUR_ANON_KEY';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
